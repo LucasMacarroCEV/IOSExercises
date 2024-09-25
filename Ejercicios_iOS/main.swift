830f3af1239318306722bc6d92dfb6adb03b0cd5
Lucas <lucas_macarro_m-tsapp1ma2324@cev.com>
2024-09-25 11:41:46 +0100

    IOS_Exercises - Exercise 1

--- Ejercicios_iOS/main.swift
+++ Ejercicios_iOS/main.swift
@@ -1,11 +1,58 @@
-//
-//  main.swift
-//  Ejercicios_iOS
-//
-//  Created by user257501 on 9/25/24.
-//
 
-import Foundation
+//-------------EXERCISE 1-------------
+//var numsArray = [Int]()
+//var sum: Int = 0
+//
+//repeat {
+//    print("Teclea un número:")
+//    if let number = Int(readLine()!){
+//        numsArray.append(number)
+//    }
+//    else{
+//        print("Error: Introduce un número y prueba de nuevo:")
+//    }
+//} while numsArray.count <= 4
+//
+//print("-------------------------------")
+//for x in numsArray{
+//    sum += x
+//    print(x)
+//}
+//print("-------------------------------")
+//print("La media de los números introducidos es: ", sum/numsArray.count)
 
-print("Hello, World!")
+//-------------EXERCISE 2-------------
+var error = false
+
+func SetName(playerNum:Int) -> String{
+    var name: String
+    repeat{
+        print("Escribe el nombre del jugador", playerNum, ":")
+        name = readLine()!
+        if name.isEmpty{
+            error = true
+            print("Error: El nombre no puede estar en blanco.")
+        }
+        else if name.count > 15{
+            error = true
+            print("Error: El nombre no puede ser tan largo.")
+        }
+        else {
+            if name.first!.isLowercase{
+                let tempChar = name.first!.uppercased()
+                name.remove(at: name.startIndex)
+                name.insert(contentsOf: tempChar, at: name.startIndex)
+            }
+            error = false
+        }
+    } while error
+    return name
+}
+
+let bullet = Int.random(in: 0..<6)
+var namePlayer1 = SetName(playerNum: 1)
+var namePlayer2 = SetName(playerNum: 2)
+print("-------------------------------")
+print("Jugador 1:", namePlayer1, "Jugador 2:", namePlayer2)
+
 
